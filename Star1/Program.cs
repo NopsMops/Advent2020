4d236975d9fc96d676b75c797a7b1e07e11f8f5a
NopsMops <NopsMops@web.de>
2020-12-01 17:22:34 +0100

    day1

---
+++ Star1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+// star 1 silver 138379
+// star 1 gold 85491920
+
+namespace Star1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\sw\AdventOfCode_2020\Advent\Input\star1_org.txt";
+            long starkey = 0;
+            if (File.Exists(path))
+            {
+                string[] fieldStr = File.ReadAllLines(path);
+                int[] query = (from string num in fieldStr select int.Parse(num)).ToArray();
+
+                foreach (int i in query)
+                {
+                    foreach (int k in query.Skip(1))
+                    {
+                        if ((k + i) == 2020)
+                        {
+                            starkey = k * i;
+                            break;
+                        }
+                    }
+                    if (starkey != 0) break;
+                }
+            }
+            Console.WriteLine("key: {0}", starkey);
+        }
+    }
+}
---
+++ Star2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Star2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\sw\AdventOfCode_2020\Advent\Input\star2_org.txt";
+            long starkey = 0;
+            if (File.Exists(path))
+            {
+                string[] fieldStr = File.ReadAllLines(path);
+                int[] query = (from string num in fieldStr select int.Parse(num)).ToArray();
+
+                foreach (int i in query)
+                {
+                    foreach (int k in query.Skip(1))
+                    {
+                        foreach (int n in query.Skip(2))
+                        {
+                            if ((k + i +n) == 2020)
+                            {
+                                starkey = k * i * n;
+                                break;
+                            }
+                        }
+                        if (starkey != 0) break;
+                    }
+                    if (starkey != 0) break;
+                }
+            }
+            Console.WriteLine("key: {0}", starkey);
+        }
+    }
+}
