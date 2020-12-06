2b4df662ad59975a9684c7758b706d9e8ab7ee8b
NopsMops <NopsMops@web.de>
2020-12-06 10:41:04 +0100

    day 6

---
+++ Star1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+// silver 6742
+// gold 3447
+
+namespace Star1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\sw\AdventOfCode_2020\Advent\Input\star1_org.txt";
+            if (File.Exists(path))
+            {
+                int sum = 0;
+                string[] lines = File.ReadAllLines(path);
+
+                HashSet<char> setchars = new HashSet<char>();
+                foreach (string item in lines)
+                {
+                    char[] cAr = item.ToCharArray();
+                    foreach( char lett in cAr)
+                    {
+                        setchars.Add(lett);
+                    }
+
+                    if (String.IsNullOrWhiteSpace(item))
+                    {
+                        sum += setchars.Count();
+                        setchars = new HashSet<char>();
+                    } 
+                }
+                Console.WriteLine("sum: {0}", sum);
+            }    
+        }
+    }
+}
---
+++ Star2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace Star2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\sw\AdventOfCode_2020\Advent\Input\star2_org.txt";
+            if (File.Exists(path))
+            {
+                int sum = 0;
+                string[] lines = File.ReadAllLines(path);
+                bool isFirst = true; 
+                HashSet<char> setFirstPerson = new HashSet<char>();
+
+                foreach (string item in lines)
+                {
+                    if (String.IsNullOrWhiteSpace(item))
+                    {
+                        sum += setFirstPerson.Count();
+                        setFirstPerson = new HashSet<char>();
+                        isFirst = true;
+                        continue;
+                    }
+
+                    HashSet<char> setchars = new HashSet<char>();
+                    char[] cAr = item.ToCharArray();
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        foreach (char lett in cAr)
+                        {
+                            setFirstPerson.Add(lett);
+                        }
+                    }
+                    else
+                    {
+                        // pepare first person list
+                        foreach( char buch in cAr)
+                        {
+                            setchars.Add(buch);
+                        }
+                        HashSet<char> removelist = new HashSet<char>();
+                        foreach (char i in setFirstPerson)
+                        {
+                            if (setchars.Contains(i) != true)
+                            {
+                                removelist.Add(i);
+                            }
+                        }
+                        foreach (char i in removelist)
+                        {
+                            setFirstPerson.Remove(i);
+                        }                      
+                    }                   
+                }
+                Console.WriteLine("sum: {0}", sum);
+            }
+        }
+    }
+}
