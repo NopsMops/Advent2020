771ef975c621e32e086877cedfd9f6374d17f961
NopsMops <NopsMops@web.de>
2020-12-15 19:19:36 +0100

    day 15

---
+++ Star1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+
+// silver 1696
+// gold 37385
+
+namespace Star1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             List<int> numbers = new List<int>() { 12, 1, 16, 3, 11, 0 };  // puzzle 12,1,16,3,11,0 -> result silver star
+            //List<int> numbers = new List<int>() { 0, 3, 6 };  // test 1 -> result 436
+
+            int currpos = numbers.Count; 
+            while (true) {
+                int diff = 0;
+                int prevval = numbers[currpos - 1];
+                for( int i = currpos - 2; i >= 0; i--) {
+                    if( numbers[i] == prevval) {
+                        diff = currpos - 1 - i;
+                        break;
+                    }
+                }
+                if( diff > 0) {
+                    // exist
+                    numbers.Add(diff);
+                }
+                else {
+                    // new number -> 0
+                    numbers.Add(0);
+                }
+                currpos++;
+                if (numbers.Count == 2020) break;
+            }
+
+            int result = numbers.Last();
+            Console.WriteLine("occupied {0}", result);
+           
+        }
+    }
+}
---
+++ Star2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+
+namespace Star2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numTurns = new Dictionary<int, int>();
+
+            // int[] input = new int[] { 0, 3, 6 };
+            int[] input = new int[] { 12, 1, 16, 3, 11, 0 };
+
+
+            for (int i = 0; i < input.Length - 1; i++) {
+                numTurns.Add(input[i], i + 1);
+            }
+
+            int lastspoken = input.Last();
+            for (int i = input.Length; i < 30000000; i++) {
+
+                if (numTurns.ContainsKey(lastspoken)) {
+                    int diff = i - numTurns[lastspoken];
+                    numTurns[lastspoken] = i;
+                    lastspoken = diff;
+                }
+                else {
+                    numTurns.Add(lastspoken, i);
+                    lastspoken = 0; // da letzter aufruf nicht vorhanden
+                }
+            }
+            //int result = numTurns.Last();
+            Console.WriteLine("lastspoken {0}", lastspoken);
+        }
+    }
+}
